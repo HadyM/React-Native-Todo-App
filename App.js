21a0dff3fb5984da814249a70f755e5330262432
Hady Mohamed <hadymohamed@pursuit.org>
2021-06-01 20:05:46 -0400

    made the splash screen last a little longer

--- App.js
+++ App.js
@@ -1,16 +1,45 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useReducer, useCallback, useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
 import Title from "./Components/Title";
 import Input from "./Components/Input";
 import List from "./Components/List";
 import { actionCreators, reducer, initialState } from "./Components/todos";
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <View>
+    <View
+      onLayout={onLayoutRootView}
+    >
       <Title>To-Do List App</Title>
       <Input
         placeholder={"Type a todo, then hit enter!..."}
